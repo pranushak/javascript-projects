617d68ec8afe1e89e57aa3c153831a42eb3d25dc
Pranusha <narsicloud@gmail.com>
2024-05-06 17:25:39 -0500

    strings part-one solved

--- stringing-characters-together/exercises/part-two.js
+++ stringing-characters-together/exercises/part-two.js
@@ -4,9 +4,11 @@ let dna = " TCG-TAC-gaC-TAC-CGT-CAG-ACT-TAa-CcA-GTC-cAt-AGA-GCT    ";
 
 // First, print out the dna strand in it's current state.
 
+console.log(dna);
+
 //1) Use the .trim() method to remove the leading and trailing whitespace, then print the result.
 
-console.log(/* Your code here. */);
+console.log(dna.trim);
 
 //2) Change all of the letters in the dna string to UPPERCASE, then print the result.
 
