a439b0dc9749a1fb20e0a960d9466c0de270414e
Pranusha <narsicloud@gmail.com>
2024-05-13 14:35:24 -0500

    printed names of array using for loop

--- loops/chapter-examples/for-Loop-Practice-With-Arrays.js
+++ loops/chapter-examples/for-Loop-Practice-With-Arrays.js
@@ -1,3 +1,7 @@
 // create an array variable containing the names
+let myFamily = ["Rian","Risha","Pranu","Narsi"];
 
 // write a for loop that prints each name on a different line
+for(let i = 0; i < myFamily.length; i++){
+    console.log(myFamily[i]);
+}
