a4358785c604d8548389919d19aafc24f7fb2ec4
Pranusha <narsicloud@gmail.com>
2024-05-13 17:51:14 -0500

    for-Loop-Exercises are solved

--- loops/exercises/for-Loop-Exercises.js
+++ loops/exercises/for-Loop-Exercises.js
@@ -3,9 +3,28 @@
     b. Print only the ODD values from 3 - 29, one number per line.
     c. Print the EVEN numbers 12 to -14 in descending order, one number per line.
     d. Challenge - Print the numbers 50 - 20 in descending order, but only if the numbers are multiples of 3. (Your code should work even if you replace 50 or 20 with other numbers). */
-
-
-
+//a.
+   for(let i = 0; i <= 20; i++){
+      console.log(i);
+   }
+//b.
+   for(let i = 3; i < 29; i++){
+    if(i%2 != 0){
+      console.log(i);
+      }
+    }
+//c.
+   for(let i = 12; i > -14; i--){
+    if(i%2 == 0){
+       console.log(i);
+     }
+   }    
+//d.
+  for(let i = 50; i >= 20; i--){
+     if(i%3 == 0){
+    console.log(i);
+     }
+  }
 
 /*Exercise #2: 
 Initialize two variables to hold the string “LaunchCode” and the array [1, 5, ‘LC101’, ‘blue’, 42].
@@ -15,10 +34,35 @@ Construct ``for`` loops to accomplish the following tasks:
   a. Print each element of the array to a new line.
   b. Print each character of the string - in reverse order - to a new line. */ 
 
+let str = "LaunchCode";
+let arr = [1, 5, 'LC101', 'blue', 42];
 
+//a.
+   for(let i = 0; i < arr.length; i++){
+    console.log(arr[i]);
+   }
 
-
-
+//b.
+  let reversed = "";
+  for(let i = 0; i < str.length; i++){
+    reversed = str[i] + reversed;
+  }
+  console.log(reversed);
+  
 /*Exercise #3:Construct a for loop that sorts the array [2, 3, 13, 18, -5, 38, -10, 11, 0, 104] into two new arrays:
   a. One array contains the even numbers, and the other holds the odds.
   b. Print the arrays to confirm the results. */
+
+  let array = [2, 3, 13, 18, -5, 38, -10, 11, 0, 104];
+  let evenArray = [];
+  let oddArray = [];
+  
+  for(let i = 0; i < array.length; i ++){
+    if(array[i]%2 == 0){
+     evenArray.push(array[i]);
+    }else{
+      oddArray.push(array[i]);
+    }
+  }
+  console.log(evenArray);
+  console.log(oddArray);
