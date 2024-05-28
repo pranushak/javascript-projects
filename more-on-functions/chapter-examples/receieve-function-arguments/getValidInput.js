dd51bd9c108e73d602ccb1436f40118c4242e93e
Pranusha <narsicloud@gmail.com>
2024-05-28 12:14:03 -0500

    VALIDATED USER INPUT

--- more-on-functions/chapter-examples/receieve-function-arguments/getValidInput.js
+++ more-on-functions/chapter-examples/receieve-function-arguments/getValidInput.js
@@ -14,8 +14,20 @@ function getValidInput(prompt, isValid) {
 
 // TODO 1: write a validator 
 // that ensures input starts with "a"
-
+let isValidInput = function(i){
+  if(i[0] === 'a'){
+    return true;
+  }
+  return false;
+};
 // TODO 2: write a validator 
 // that ensures input is a vowel
-
+let isVowel = function(isValidInput){
+  if(isValidInput === true){
+    return 'its vowel';
+  }
+  return 'not a vowel';
+}
 // Be sure to test your code!
+console.log(getValidInput('Enter a string: ',isValidInput));
+console.log(getValidInput('Enter a word: ', isVowel));
