7e66ffa8f181be3207bd72585e5d53b0993e455a
Pranusha <narsicloud@gmail.com>
2024-06-03 15:01:22 -0500

    object exrecises

--- objects-and-math/exercises/ObjectExercises.js
+++ objects-and-math/exercises/ObjectExercises.js
@@ -12,6 +12,26 @@ let salamander = {
    age: 5
 };
 
+let chimpTwo = {
+   name : "Brad",
+   species : "Chimpanzee",
+   mass : 11,
+   age : 6
+};
+
+let beagle = {
+   name : "Leroy",
+   species : "Beagle",
+   mass : 14,
+   age : 5
+};
+
+let tardigrade = {
+   name : "Almina",
+   species : "Tardigrade",
+   mass : 0.0000000001,
+   age : 1
+};
 
 // After you have created the other object literals, add the astronautID property to each one.
 
@@ -22,3 +42,24 @@ let salamander = {
 // Print out the relevant information about each animal.
 
 // Start an animal race!
+
+let arrayOfAnimals = [superChimpOne,salamander,chimpTwo,beagle,tardigrade];
+
+for(let i = 0; i < arrayOfAnimals.length; i++){
+   arrayOfAnimals[i]["astronautID"] = Math.trunc((Math.random() * 10) + 1);
+    
+ }
+
+ function crewReports(arrayOfAnimals){
+   let report = '';
+   for(let i = 0; i < arrayOfAnimals.length; i++){
+      report += '\n'+`${arrayOfAnimals[i].name} is a ${arrayOfAnimals[i].species}. They are ${arrayOfAnimals[i].age} years old and ${arrayOfAnimals[i].mass} kilogram. Their ID is ${arrayOfAnimals[i].astronautID}.`
+   }
+   return report;
+ }
+
+ function fitnessTest(arr){
+   
+ }
+
+ console.log(crewReports(arrayOfAnimals));
