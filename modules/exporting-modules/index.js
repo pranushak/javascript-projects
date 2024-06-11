10f7c29d78e6e07456bcd0b54283d6a0bcf3a5f2
Pranusha <narsicloud@gmail.com>
2024-06-11 09:56:49 -0500

    practiceExports

--- modules/exporting-modules/index.js
+++ modules/exporting-modules/index.js
@@ -1,1 +1,11 @@
 // Import the modules exported from practiceExports.js below:
+
+const practice = require('./practiceExports.js');
+
+console.log(typeof practice);
+
+let arr = ['Hello', 'World', 123, 987, 'LC101'];
+
+console.log(practice.isPalindrome('mom'));
+console.log(practice.evenOrOdd(9));
+console.log(practice.randomArrayElement(arr))
