8b8a5152c74f367cfbc37bedb14983716c0ea427
Pranusha <narsicloud@gmail.com>
2024-07-31 23:22:40 -0500

    dom-and-events

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -4,7 +4,23 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
-    
+
+    //console.log(p.innerHTML)
+    button.addEventListener('click', event => {
+    paragraph.innerHTML = 'Houston! We have liftoff!';
+    });
+    missionAbort.addEventListener('mouseover', event => {
+        event.target.style.backgroundColor = "red";
+    });
+    missionAbort.addEventListener('mouseout', event => {
+        event.target.style.backgroundColor = "";
+    });
+    missionAbort.addEventListener('click', event => {
+        let c = confirm("Are you sure you want to abort the mission?.")
+        if(c) {
+            paragraph.innerHTML = 'Mission aborted! Space shuttle returning home.'
+        }
+    })
 }
 
 window.addEventListener("load", init);
